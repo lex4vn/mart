26d6efe9d192ef5312fdc0f387ee3597c15da0c7
ifindthanh <thanhx0kjt@hotmail.com>
2016-01-04 18:07:16 +0700

    Update pavblog.php

--- opencart/admin/language/vietnamese/module/pavblog.php
+++ opencart/admin/language/vietnamese/module/pavblog.php
@@ -11,161 +11,161 @@
 $_['heading_title'] = 'Pav Blog Management';
 
 // Text
-$_['text_treemenu'] = 'Tree Category Management';
- $_['text_menu_assignment'] = 'Module Assignment';
+$_['text_treemenu'] = 'Quản lý cây category';
+ $_['text_menu_assignment'] = 'Phân công module';
 $_['text_module']         = 'Modules';
-$_['text_success']        = 'Success: You have modified module Category!';
-$_['text_content_top']    = 'Content Top';
-$_['text_content_bottom'] = 'Content Bottom';
-$_['text_column_left']    = 'Column Left';
-$_['text_column_right']   = 'Column Right';
-$_['text_mainmenu']   = 'Main Menu';
+$_['text_success']        = 'Chúc mừng: Bạn vừa cập nhật thành công module Category!';
+$_['text_content_top']    = 'Nội dung bên trên';
+$_['text_content_bottom'] = 'Nội dung bên dưới';
+$_['text_column_left']    = 'Nội dung bên trái';
+$_['text_column_right']   = 'Nội dung bên phải';
+$_['text_mainmenu']   = 'Menu chính';
 $_['text_slideshow']   = 'Slideshow';
 $_['text_promotion']   = 'Promotion';
 $_['text_mass_bottom'] = 'Mass Bottom';
-$_['text_bottom1']   = 'Bottom 1';
-$_['text_bottom2']   = 'Bottom 2';
-$_['text_bottom3']   = 'Bottom 3';
-$_['text_footer_top']   = 'Footer Top';
-$_['text_footer_center']   = 'Footer Center';
-$_['text_footer_bottom']   = 'Footer Bottom';
-$_['text_create_new'] = 'Create New Category';
-$_['text_edit_menu'] = 'Edit: %s  (ID:%s)';
-$_['all_page']   = 'All Page';
+$_['text_bottom1']   = 'Bên dưới 1';
+$_['text_bottom2']   = 'Bên dưới 2';
+$_['text_bottom3']   = 'Bên dưới 3';
+$_['text_footer_top']   = 'Footer bên trên';
+$_['text_footer_center']   = 'Footer giữa';
+$_['text_footer_bottom']   = 'Footer bên dưới';
+$_['text_create_new'] = 'Thêm mới Category';
+$_['text_edit_menu'] = 'Chỉnh sửa : %s  (ID:%s)';
+$_['all_page']   = 'Tất cả các trang';
 $_['text_seo_data'] = 'SEO Data';
-$_['entry_keyword'] = '<b>SEO Keyword</b>';
+$_['entry_keyword'] = '<b>Từ khóa SEO</b>';
 
-$_['help_entry_keyword'] = '<i>*Do not use spaces instead replace spaces with - and make sure the keyword is globally unique.</i>';
+$_['help_entry_keyword'] = '<i>*Không sửa dụng khoảng trắng hoặc thay nó bởi ký tự - và đảm bảo từ khóa là hoàn toàn duy nhất.</i>';
 
 $_['text_keyword_listing_blogs_page'] = 'Key Word - Listing Blog Page<p><i>Keyword using for listing blog by tag or by user</i>:<br> Example: http://www.domain.com/blogs?tag=opencart</p>';
 
-$_['text_image_manager']   = 'Images Management';
-$_['text_front_page'] = 'Front Page';
-$_['text_category_page'] = 'Category Page';
-$_['text_item_page'] = 'Item Page';
-$_['text_filter_blog'] = 'Filter Blogs';
-$_['text_normal'] = 'Normal';
+$_['text_image_manager']   = 'Quản lý ảnh';
+$_['text_front_page'] = 'Trang phía trước';
+$_['text_category_page'] = 'Trang category';
+$_['text_item_page'] = 'Trang item';
+$_['text_filter_blog'] = 'Bộ lọc Blogs';
+$_['text_normal'] = 'Bình thường';
 $_['text_seo'] = 'SEO';
-$_['text_guide_urls'] = 'Guides';
-$_['text_guide_seo'] = 'How to Enable SEO';
-$_['text_guide_category'] = 'Manage Category';
-$_['text_guide_blog'] = 'Manage Blog';
-$_['text_guide_module_setting'] = 'Module Setting';
+$_['text_guide_urls'] = 'Hướng dẫn';
+$_['text_guide_seo'] = 'Hướng dẫn kích hoạt SEO';
+$_['text_guide_category'] = 'Quản lý Category';
+$_['text_guide_blog'] = 'Quản lý Blog';
+$_['text_guide_module_setting'] = 'Cài đặt module';
 // Entry
-$_['entry_banner']        = 'Banner:';
-$_['entry_dimension']     = 'Dimension (W x H) and Resize Type:';
+$_['entry_banner']        = 'Biểu ngữ:';
+$_['entry_dimension']     = 'Kích thước (Dài x Cao) và kiểu Resize:';
 $_['entry_layout']        = 'Layout:';
-$_['entry_position']      = 'Position:';
-$_['entry_status']        = 'Status:';
-$_['entry_sort_order']    = 'Sort Order:';
-$_['entry_description']    = 'Description:';
+$_['entry_position']      = 'Vị trí:';
+$_['entry_status']        = 'Trạng thái:';
+$_['entry_sort_order']    = 'Thứ tự sắp xếp:';
+$_['entry_description']    = 'Mô tả:';
 $_['entry_type']    = 'Type:';
 $_['entry_url']    = 'URL:';
 $_['entry_category']    = 'Category:';
-$_['entry_product']    = 'Product:';
-$_['entry_manufacturer']    = 'Manufacturer:';
-$_['entry_information']    = 'Information:';
+$_['entry_product']    = 'Sản phẩm:';
+$_['entry_manufacturer']    = 'Nhà sản xuất:';
+$_['entry_information']    = 'Thông tin:';
 $_['entry_html']    = 'HTML:';
 $_['entry_parent_id']    = 'Parent ID:';
 $_['entry_menuclass']    = 'Menu Class:';
-$_['entry_showtitle']    = 'Show Title:';
+$_['entry_showtitle']    = 'Hiển thị tiêu đề:';
 $_['entry_isgroup']    = 'Is Group:';
 $_['entry_iscontent']    = 'Is Content:';
-$_['entry_default_column_width']    = 'Default Column Width (SpanX): ';
+$_['entry_default_column_width']    = 'Chiều rộng mặc định cột (SpanX): ';
 $_['entry_columns']    = 'Columns';
-$_['entry_detail_columns'] = 'Detail Columns Width';
+$_['entry_detail_columns'] = 'Chi tiết chiều rộng cột';
 $_['entry_sub_menutype'] = 'Sub Menu Type';
-$_['entry_submenu_content'] = 'Submenu Content';
+$_['entry_submenu_content'] = 'Nội dung menu con';
 $_['text_none'] = 'None';
-$_['text_explain_input_html'] = 'If menu content is HTML so all submenu of this menu will not be showed';
-$_['text_explain_input_auto'] = 'Type character or word to search';
-$_['text_explain_submenu_cols'] = 'Enter detail width of each subcols in values 1->12.<br>Example: col1=3 col3=5';
-$_['text_explain_submenu_type'] = 'If the type is Menu, so submenus of this will be showed';
-$_['text_browse'] = 'Browser';
+$_['text_explain_input_html'] = 'Nếu nội dung của menu là HTML thì các menu con của nó sẽ không được hiển thị';
+$_['text_explain_input_auto'] = 'Nhập ký tự hoặc từ cần tìm kiếm';
+$_['text_explain_submenu_cols'] = 'Nhập chi tiết chiều rộng cho từng cột con trong khoảng giá trị từ 1 đến 12.<br>Ví dụ: col1=3 col3=5';
+$_['text_explain_submenu_type'] = 'Nếu được set là menu, các menu con của nó sẽ được hiện thị';
+$_['text_browse'] = 'Duyệt';
 $_['text_clear'] = 'Clear';
-$_['entry_publish'] = 'is Published'; 
+$_['entry_publish'] = 'Đã được xuất bản'; 
 
 
-$_['button_save_edit']    = "Save And Edit";
-$_['button_save_new']   = 'Save And Create New';
+$_['button_save_edit']    = "Lưu và chỉnh sửa";
+$_['button_save_new']   = 'Lựu và tạo mới';
 
 
 //
-$_['message_delete'] = "Are you sure to delete this";
+$_['message_delete'] = "Bạn có thực sự muốn xóa?";
 // Error
-$_['error_permission']    = 'Warning: You do not have permission to modify module Category!';
-$_['error_dimension']     = 'Width &amp; Height dimensions required!';
-$_['error_missing_title'] = 'Please Enter Menu Title in each language tabs';
-$_['text_explain_drapanddrop'] = 'To sort orders or update parent-child, you drap and drop expected menu, then click to Update button to Save';
+$_['error_permission']    = 'Cảnh bảo: Bạn không có quyền chỉnh sửa module Category!';
+$_['error_dimension']     = 'Bắt buộc phải nhập chiều dài &amp; cao!';
+$_['error_missing_title'] = 'Vui lòng nhập tiêu đề menu cho từng ngôn ngữ';
+$_['text_explain_drapanddrop'] = 'Để sắp xếp thứ tự, kéo và thả menu tương ứng và nhấn nút "Cập nhật" để lưu lại';
 /***/
 $_['menu_dashboard'] = "Dashboard";
 $_['menu_manage_categories'] = "Categories";
-$_['menu_manage_blogs'] = "Manage Blogs";
-$_['menu_addblog'] = "Add Blog";
-$_['menu_comment'] = "Comments";
-$_['menu_module_setting'] = "General Setting";
-$_['menu_information'] = "Info";
+$_['menu_manage_blogs'] = "Quản lý Blogs";
+$_['menu_addblog'] = "Thêm Blog";
+$_['menu_comment'] = "Các bình luận";
+$_['menu_module_setting'] = "Cài đặt chung";
+$_['menu_information'] = "Thông tin";
 /* DASHBOARD PAGE **/
 $_['panel_page_heading_title'] = 'Dashboard';
 $_['text_quickicons'] = 'Quick Icons';
-$_['module_latest'] = 'Blog Latest';
+$_['module_latest'] = 'Blog mới nhất';
 $_['module_comment'] = 'Blog Comment';
 $_['module_category'] = 'Blog Category Menu';
-$_['text_modules_urls'] = 'URLs Format';
+$_['text_modules_urls'] = 'Định dạng URLs';
 
-$_['latest_blog'] = 'Latest Blog';
-$_['most_read'] = 'Most Read';
-$_['latest_comment'] = 'Latest Comment';
+$_['latest_blog'] = 'Blog gần nhất';
+$_['most_read'] = 'Đọc nhiều nhất';
+$_['latest_comment'] = 'Bình luận gần nhất';
 
 $_['databroad_categories'] = 'Categories';
-$_['databroad_add_category'] = 'Add Category';
+$_['databroad_add_category'] = 'Thêm Category';
 $_['databroad_blogs'] = 'Blogs';
-$_['databroad_add_blog'] = 'Add Blog';
-$_['databroad_comment'] = 'Comment';
-$_['databroad_modules_setting'] = 'Modules Setting';
+$_['databroad_add_blog'] = 'Thêm Blog';
+$_['databroad_comment'] = 'Bình luận';
+$_['databroad_modules_setting'] = 'Cài đặt modules';
 
 /** CATEGORY PAGE **/
-$_['category_page_heading_title'] = "Categories Management";
+$_['category_page_heading_title'] = "Quản lý categorys";
 $_['text_tree_category_menu'] = "Categories Tree Menu"; 
-$_['text_category_information'] = "Information"; 
-$_['text_filter_title'] = 'Filter Title';
+$_['text_category_information'] = "Thông tin"; 
+$_['text_filter_title'] = 'Lọc theo tiêu đề';
 $_['text_reset'] = 'Reset';
-$_['text_position'] = 'Position';
+$_['text_position'] = 'Vị trí';
 /** BLOGS PAGE **/
-$_['blogs_page_heading_title'] = 'Blogs Managements';
-$_['entry_title'] = "Title";
-$_['button_create_blog'] = 'Create Blog';
-$_['text_created'] = 'Created Date';
-$_['text_hits'] = 'Hits';
+$_['blogs_page_heading_title'] = 'Quản lý các blog';
+$_['entry_title'] = "Tiêu đề";
+$_['button_create_blog'] = 'Tạo blog';
+$_['text_created'] = 'Ngày tạo';
+$_['text_hits'] = 'Lượt xem';
 $_['entry_tags'] = 'Tags';
 $_['text_explain_tags'] = 'example: fashion, smart,funny';
 $_['text_facebook_id_explain'] = 'Register A Comment Box, Then Get Application ID in Script Or Register Facebook Application ID to moderate comments';
 
-$_['text_error'] = 'Sorry, We are updating, please come back late. Thanks';
+$_['text_error'] = 'Rất tiếc, Chúng tôi đang tiến hành update, vui lòng quay lại sau. Xin cảm ơn';
 /** BLOG PAGE **/
-$_['blog_page_heading_title'] = "Blog Edit/Create New"; 
+$_['blog_page_heading_title'] = "Chỉnh sửa/Thêm mới blog"; 
 $_['text_general'] = "General"; 
-$_['text_gallery'] = "Images - Gallery"; 
+$_['text_gallery'] = "Thư viện ảnh - Phòng trưng bày"; 
 $_['text_meta']   = "Meta - SEO";
 $_['entry_category_id'] = 'Category';
-$_['entry_hits'] = 'Hits';
-$_['entry_created'] = 'Created Date';
-$_['entry_creator'] = 'Creator';
-$_['entry_content'] = 'Content';
-$_['entry_image'] = 'Image';
-$_['entry_video_code'] = 'Video Code (Youtube Code, Vimeo Code)';
+$_['entry_hits'] = 'Lượt xem';
+$_['entry_created'] = 'Ngày tạo';
+$_['entry_creator'] = 'Người tạo';
+$_['entry_content'] = 'Nội dung';
+$_['entry_image'] = 'Ảnh';
+$_['entry_video_code'] = 'Mã video (Youtube Code, Vimeo Code)';
 $_['entry_meta_title'] = 'Meta Title';
 $_['entry_meta_keyword'] = 'Meta Keyword';
 $_['entry_meta_description'] = 'Meta Description';
 /**COMMENTS PAGE **/
-$_['comments_page_heading_title'] = 'Comments Management';
-$_['text_comment'] = 'Comment';
-$_['text_blog_title'] = 'Blog Title';
+$_['comments_page_heading_title'] = 'Quản lý các bình luận';
+$_['text_comment'] = 'Bình luận';
+$_['text_blog_title'] = 'Tiêu đề blog';
 $_['text_username'] = 'User';
 $_['text_email'] = 'Email';
-$_['button_publish'] = 'publish';
-$_['button_unpublish'] = 'unpublish';
-$_['text_status'] = 'Status';
+$_['button_publish'] = 'xuất bản';
+$_['button_unpublish'] = 'hủy xuất bản';
+$_['text_status'] = 'Trạng thái';
 $_['text_enable'] = 'Enable';
 $_['text_confirm_delete'] = 'Are you sure to  delete this?';
 $_['text_disable'] = 'Disable';
@@ -173,41 +173,41 @@ $_['text_enable'] = 'Enable';
 
 
 /** GENERAL SETTING **/
-$_['entry_children_columns'] = 'Children Columns';
-$_['entry_category_image_demension'] = 'Category Image Demension (Width x Height) ';
+$_['entry_children_columns'] = 'Các cột con';
+$_['entry_category_image_demension'] = 'Category Image Demension (Dài x Cao) ';
 $_['modules_page_heading_title'] = 'General & Modules Setting';
-$_['text_general_setting'] = 'General Setting';
-$_['entry_large_image_demension'] = 'Blog - Large Image Demension (Width x Height)';
-$_['entry_small_image_demension'] = 'Blog - Small Image Demension (Width x Height)';
-$_['entry_xsmall_image_demension'] = 'Blog - XSmall Image Demension (Width x Height)';
+$_['text_general_setting'] = 'Cài đặt chung';
+$_['entry_large_image_demension'] = 'Blog - Kích cỡ ảnh lớn (Dài x Cao)';
+$_['entry_small_image_demension'] = 'Blog - Kích cỡ ảnh nhỏ (Dài x Cao)';
+$_['entry_xsmall_image_demension'] = 'Blog - Kích cỡ ảnh siêu nhỏ (Dài x Cao)';
 $_['entry_rss_limit'] = 'Limit Blogs In RSS';
 $_['text_category_setting'] = 'Category Page Setting';
 $_['entry_limit_leading_blogs'] = 'Limit Leading Blogs';
 $_['entry_limit_secondary_blogs 	'] = 'Limit Secondary Blogs';
 $_['entry_leading_image_types'] = 'Image Types';
-$_['text_large_image'] = 'Large Image';
-$_['text_small_image'] = 'Small Image';
+$_['text_large_image'] = 'Ảnh lớn';
+$_['text_small_image'] = 'Ảnh nhỏ';
 $_['entry_limit_secondary_blogs'] = 'Limit Secondary Blogs';
 $_['entry_secondary_image_types'] = 'Secondary Image Types';
-$_['entry_show_title'] = 'Show Title';
-$_['entry_show_image'] = 'Show Image';
-$_['entry_show_author'] = 'Show Author';
-$_['entry_show_category'] = 'Show Category';
-$_['entry_show_created'] = 'Show Created Date';
-$_['entry_show_comment_counter'] = 'Show Comment Counter';
-$_['entry_show_description'] = 'Show Description' ;
-$_['entry_show_readmore'] = 'Show Readmore' ;
-$_['entry_show_hits'] = 'Show Hits' ;
-$_['text_blog_setting'] = 'Blog Setting';
+$_['entry_show_title'] = 'Hiển thị tiêu đề';
+$_['entry_show_image'] = 'Hiển thị ảnh';
+$_['entry_show_author'] = 'Hiển thị tác giả';
+$_['entry_show_category'] = 'Hiển thị Category';
+$_['entry_show_created'] = 'Hiển thị ngày tạo';
+$_['entry_show_comment_counter'] = 'Hiển thị số bình luận';
+$_['entry_show_description'] = 'Hiển thị mô tả' ;
+$_['entry_show_readmore'] = 'Hiển thị Readmore' ;
+$_['entry_show_hits'] = 'Hiển thị lượt xem' ;
+$_['text_blog_setting'] = 'Cài đặt blog';
 $_['entry_blog_image_types'] = 'Blog Image Type';
 $_['blog_secondary_image_types'] = 'Secondary Image Types';
-$_['blog_show_title'] = 'Show Title';
-$_['blog_show_image'] = 'Show Image';
-$_['blog_show_author'] = 'Show Author';
-$_['blog_show_category'] = 'Show Category';
-$_['blog_show_created'] = 'Show Created Date';
-$_['blog_show_comment_counter'] = 'Show Comment Counter';
-$_['entry_show_comment_form'] = 'Show Comment Form';
+$_['blog_show_title'] = 'Hiển thị tiêu đề';
+$_['blog_show_image'] = 'Hiển thị ảnh';
+$_['blog_show_author'] = 'Hiển thị tác giả';
+$_['blog_show_category'] = 'Hiển thị Category';
+$_['blog_show_created'] = 'Hiển thị ngày tạo';
+$_['blog_show_comment_counter'] = 'Hiển thị số bình luận';
+$_['entry_show_comment_form'] = 'Hiển thị form bình luận';
 
 $_['text_modules_setting'] = 'Modules Setting';
 
@@ -242,16 +242,16 @@ $_['error_missing_description'] = 'Missing Blog Description';
 $_['error_missing_content'] = 'Missing Blog Content';
 
 
-$_['text_save'] = 'save';
+$_['text_save'] = 'lưu';
 $_['text_success_update_tree'] = 'Success: You have modified Tree-Items!';
-$_['text_edit'] = 'Edit';
+$_['text_edit'] = 'Sửa';
 
 $_['entry_user'] = 'User';
 $_['entry_email'] = 'Email';
-$_['entry_comment'] = 'Comment';
+$_['entry_comment'] = 'Bình luận';
 
 // Pavblogcategory
-$_['button_module_add'] = 'Add module';
+$_['button_module_add'] = 'Thêm module';
 
 // PavblogComment
 $_['entry_carousel']    = 'Limit Items:';
@@ -260,9 +260,9 @@ $_['entry_carousel']    = 'Limit Items:';
 $_['entry_prefixclass'] = 'Prefix Class';
 $_['entry_tabs'] = 'Type List Products';
 
-$_['text_latest'] = 'Latest';
+$_['text_latest'] = 'Mới nhất';
 $_['text_featured'] = 'Featured';
-$_['text_mostviewed'] = 'Most View';
+$_['text_mostviewed'] = 'Xem nhiều nhất';
 
 $_['create_module'] = 'Create Module';
 $_['entry_module_name'] = 'Module Name';
